1b35bc1868ca7dbccd2c4fcc3d8e0751e51ba557
Alex <seven37@yandex.ru>
2019-11-17 01:11:37 +0400

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,14 @@
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0])
+
+let answer = confirm("Are you here?");
+
+console.log(answer);
